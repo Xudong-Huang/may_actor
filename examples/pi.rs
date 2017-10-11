1fb5880742eb948eb287255c07f1fc6aaf5cf1f3
Xudong Huang <huangxu008@hotmail.com>
2017-10-11 09:46:56 +0800

    :pencil: refine pi example

--- examples/pi.rs
+++ examples/pi.rs
@@ -5,7 +5,7 @@ use may::coroutine;
 use may::sync::mpsc;
 use may_actor::Actor;
 
-const TOTAL_NUM: usize = 100000000;
+const TOTAL_NUM: usize = 100_000_000;
 const WORK_LOAD: usize = 10000;
 const ACTOR_NUMBER: usize = 100;
 
@@ -18,12 +18,12 @@ fn calc_work(start: usize, end: usize) -> f64 {
     for i in start..end {
         sum += pow_of_minus_one(i) / (2.0 * i as f64 + 1.0)
     }
-    sum * 4.0
+    sum
 }
 
 // single thread version for pi
 fn pi() -> f64 {
-    calc_work(0, TOTAL_NUM)
+    calc_work(0, TOTAL_NUM) * 4.0
 }
 
 // coroutine version for pi
@@ -43,12 +43,20 @@ fn pi_coroutine() -> f64 {
     for _ in 0..works {
         sum += rx.recv().unwrap();
     }
-    sum
+    sum * 4.0
 }
 
 // actor version for pi
 fn pi_actor() -> f64 {
     const WORKS: usize = TOTAL_NUM / WORK_LOAD;
+
+    struct Worker;
+    impl Worker {
+        fn work(&self, master: Actor<Master>, start: usize, end: usize) {
+            let data = calc_work(start, end);
+            master.call(move |me| me.recv_data(data));
+        }
+    }
 
     struct Master {
         pi: f64,
@@ -57,21 +65,34 @@ fn pi_actor() -> f64 {
     }
 
     impl Master {
+        fn start(&self) {
+            // create the worker actors
+            let mut workers = vec![];
+            for _ in 0..ACTOR_NUMBER {
+                workers.push(Actor::new(Worker));
+            }
+
+            // send work load to workers
+            let mut start = 0;
+            let mut idx = 0;
+            for _ in 0..WORKS {
+                let end = start + WORK_LOAD;
+                let master = unsafe { Actor::from(self) };
+                workers[idx].call(move |me| me.work(master, start, end));
+                start = end;
+                idx += 1;
+                if idx == ACTOR_NUMBER {
+                    idx = 0;
+                }
+            }
+        }
+
         fn recv_data(&mut self, data: f64) {
             self.pi += data;
             self.count += 1;
             if self.count == WORKS {
                 self.tx.send(self.pi).unwrap();
             }
-        }
-    }
-
-    struct Worker;
-
-    impl Worker {
-        fn work(&self, master: Actor<Master>, start: usize, end: usize) {
-            let data = calc_work(start, end);
-            master.call(move |me| me.recv_data(data));
         }
     }
 
@@ -84,27 +105,9 @@ fn pi_actor() -> f64 {
         tx: tx,
     });
 
-    // create the worker actors
-    let mut workers = vec![];
-    for _ in 0..ACTOR_NUMBER {
-        workers.push(Actor::new(Worker));
-    }
+    master.call(|me| me.start());
 
-    // send work load to workers
-    let mut start = 0;
-    let mut idx = 0;
-    for _ in 0..WORKS {
-        let end = start + WORK_LOAD;
-        let master = master.clone();
-        workers[idx].call(move |me| me.work(master, start, end));
-        start = end;
-        idx += 1;
-        if idx == ACTOR_NUMBER {
-            idx = 0;
-        }
-    }
-
-    rx.recv().unwrap()
+    rx.recv().unwrap() * 4.0
 }
 
 fn main() {
